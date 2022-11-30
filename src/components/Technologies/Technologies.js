e951be0a62b448f9cab83f4f6a009e9f8a859362
MRDJsekhar <saisekhar77492@gmail.com>
2022-11-30 11:47:11 +0530

    master portfolio completed

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend, DiLinux, DiJava } from 'react-icons/di';
+import { DiReact, DiLinux, DiJava } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () => (
   <Section id="tech">
     <SectionDivider />
+    <br/>
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
       I've worked with a range a technologies in the web development world.
